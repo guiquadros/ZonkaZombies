51bf98a9c747e69badbf2e23e7883b271e459669
andreirs <andreirs@outlook.com>
2017-03-24 14:26:10 -0300

    Enemies that walk and creep follow a target Transform through NavMesh

---
+++ Assets/Scripts/TinyCrew/Prototype/Enemy/CrawlerEnemyBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace TinyCrew.Prototype.Enemy
+{
+    public class CrawlerEnemyBehaviour : EnemyBehaviour
+    {
+        [SerializeField, Range(.5f, 4)]
+        private float _velocityMultiplier = 2;
+        [SerializeField]
+        private AnimationCurve _animationCurve;
+
+        private float _agentMaximmumSpeed;
+
+        private void Start()
+        {
+            _agentMaximmumSpeed = agent.speed;
+        }
+
+        protected override void Update()
+        {
+            float currentDelta = Time.time * _velocityMultiplier;
+
+            float currentAgentSpeed = Mathf.Sin(currentDelta);
+
+            currentAgentSpeed = Mathf.Abs(currentAgentSpeed);
+
+            // Dynamically sets the agent's speed based on the Animation Curve, to make the creeping effect
+            agent.speed = _animationCurve.Evaluate(currentAgentSpeed) * _agentMaximmumSpeed;
+
+            base.Update();
+        }
+    }
+}
