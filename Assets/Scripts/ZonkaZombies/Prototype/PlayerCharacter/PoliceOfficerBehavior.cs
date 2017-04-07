49ea922d1424084315cf8b90722cb8605a310739
Guilherme Quadros da Silva <guiquadros35@gmail.com>
2017-04-06 21:43:04 -0300

    player character rotation interpolation + movement speed increased

--- Assets/Scripts/ZonkaZombies/Prototype/PlayerCharacter/PoliceOfficerBehavior.cs
+++ Assets/Scripts/ZonkaZombies/Prototype/PlayerCharacter/PoliceOfficerBehavior.cs
@@ -20,9 +20,19 @@ namespace ZonkaZombies.Prototype.PlayerCharacter
         [SerializeField]
         private Transform _bodyTransform;
 
+        [SerializeField]
+        private float _rotationCounter = 0.0f;
+        private float _rotationDelta = 0.0f;
+
+        [SerializeField]
+        [Range(0, 1440)]
+        private float _angularSpeed = 720.0f;
+
         private Vector3 _movement;
         private InputReader _inputReader;
         private Rigidbody _characteRigidbody;
+        private Quaternion _startRotation = Quaternion.identity;
+        private Quaternion _endRotation = Quaternion.identity;
 
         private void Awake()
         {
@@ -51,9 +61,31 @@ namespace ZonkaZombies.Prototype.PlayerCharacter
         {
             //rotation
             Vector3 rotationDirection = new Vector3(-_inputReader.RightAnalogStickHorizontal(), 0f, -_inputReader.RightAnalogStickVertical());
-            if (rotationDirection == Vector3.zero) return;
-            Quaternion rotation = Quaternion.LookRotation(rotationDirection);
-            _bodyTransform.rotation = rotation;
+
+            if (rotationDirection != Vector3.zero) 
+            {
+                _rotationCounter = 0.0f;
+                _startRotation = transform.rotation;
+                _endRotation = Quaternion.LookRotation(rotationDirection);
+
+                float startY = _startRotation.eulerAngles.y;
+                float endY = _endRotation.eulerAngles.y;
+
+                _rotationDelta = Mathf.Abs(startY - endY);
+                float result = Mathf.Abs(startY - (endY - 360f));
+
+                if (result < _rotationDelta)
+                    _rotationDelta = result;
+                result = Mathf.Abs(startY - (endY + 360f));
+
+                if (result < _rotationDelta)
+                    _rotationDelta = result;
+
+                if (_rotationDelta <= 0.01f) return;
+            }
+
+            _rotationCounter += Time.deltaTime * _angularSpeed / _rotationDelta;
+            transform.rotation = Quaternion.Lerp(_startRotation, _endRotation, _rotationCounter);
         }
 
         private void HandleGunFire()
