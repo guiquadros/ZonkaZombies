35a32880e95058355ed2f71c4bab00df7697cca1
Ândrei Schuch <andreirs@outlook.com>
2017-04-24 20:47:10 -0300

    Changed Awake method visibility in SingletonMonoBehaviour<T> class to 'protected virtual'

--- Assets/Scripts/ZonkaZombies/Util/Singleton.cs
+++ Assets/Scripts/ZonkaZombies/Util/Singleton.cs
@@ -6,7 +6,7 @@ namespace ZonkaZombies.Util
     {
         public static T Instance { get; protected set; }
 
-        private void Awake()
+        protected virtual void Awake()
         {
             Instance = FindObjectOfType<T>();
         }
