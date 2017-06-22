35272246507d97614417bca49263444f757a74f1
Guilherme Quadros da Silva <guiquadros35@gmail.com>
2017-06-22 04:35:32 -0300

    City - fix collisors + added camera, split handler and theme objs + integrated with the game (start)

---
+++ Assets/Scripts/ZonkaZombies/Scenes/GoToNextSceneDialogueScientist.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FreeAssets.Scripts;
+using UnityEngine;
+using ZonkaZombies.Input;
+using ZonkaZombies.Managers;
+using ZonkaZombies.UI;
+using ZonkaZombies.UI.Data;
+
+namespace ZonkaZombies.Scenes
+{
+    public class GoToNextSceneDialogueScientist : MonoBehaviour
+    {
+        private bool _isReady = false;
+
+        [SerializeField]
+        private DialogueHandler _dialogueHandler;
+
+        private void Start()
+        {
+            _dialogueHandler.DialogueFinished += OnDialogueFinished;
+        }
+        
+        private void OnDestroy()
+        {
+            _dialogueHandler.DialogueFinished -= OnDialogueFinished;
+        }
+
+        private void OnDialogueFinished()
+        {
+            _isReady = true;
+        }
+
+        private void Update()
+        {
+            if (_isReady)
+            {
+                _isReady = false;
+                SceneController.Instance.FadeAndLoadScene(GameScenes.CITY);
+            }
+        }
+    }
+}
